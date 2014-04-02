f4725983944d57c7b75a90bde535a8725e26fc78
Matthew Barksdale <mattwb65@gmail.com>
2014-04-02 22:46:38 +0000

    Made AthemeWeb into a class, added configuration file

--- athemeweb-2.rb
+++ athemeweb-2.rb
@@ -1,8 +1,20 @@
 #!/usr/bin/env ruby
 
-require 'sinatra'
+require 'sinatra/base'
 require 'haml'
+require 'yaml'
 
-get '/' do
-  haml :placeholder, :format => :html5
+class AthemeWeb < Sinatra::Base
+
+  @@config = YAML.load_file("athemeweb.conf")
+  
+  enable :sessions
+  set :bind => @@config['app']['host'], :port => @@config['app']['port'], :session_secret => @@config['app']['secret']
+
+  get '/' do
+    haml :placeholder, :format => :html5
+  end
+
 end
+
+AthemeWeb.run!
