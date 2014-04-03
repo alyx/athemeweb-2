82a873e714e39e2a7b3d9f75bd2907e8933b12c1
Matthew Barksdale <mattwb65@gmail.com>
2014-04-03 00:16:25 +0000

    Move AthemeWeb to thin instead of WEBrick. This allows us to take make Sinatra asynchronous

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source "https://rubygems.org"
 
 gem 'sinatra', require: 'sinatra/base'
 gem 'haml'
+gem 'async_sinatra'
+gem 'eventmachine'
+gem 'thin'
--- app.rb
+++ app.rb
@@ -7,14 +7,17 @@ $: << File.expand_path('../', __FILE__)
 $: << File.expand_path('../lib', __FILE__)
 
 require 'app/routes/main'
+
 module AthemeWeb
   class App < Sinatra::Application
     @@config = YAML.load_file('athemeweb.conf')
     configure do
+      register Sinatra::Async
       disable :method_override
       disable :static
       set :bind, @@config['app']['host']
       set :port, @@config['app']['port']
+      set :server, :thin
       set :sessions,
         :httponly     => true,
         :secure       => production?,
